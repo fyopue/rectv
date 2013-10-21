34f80a0f9bd1dd4c2f89384e05ca1536b92e4e68
Mahoroba Shinshi <fyopue@gmail.com>
2013-10-21 10:29:53 +0900

    Create rectv.sh
    完全自動化版

---
+++ rectv.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# $1 -- ch_num
+# $2 -- rec_min
+# $3 -- title
+# $4 -- iepg seed
+settingfile=/filedir/
+case "${1}" in
+  "103" | "910" ) sid=${1} ;;
+  * ) sid=hd ;;
+esac
+g="grep"
+dt=`date +%y%m%d%H%M%S`
+mt=`echo $(( 60 * ${2} ))`
+# hdd-id
+d1="hdd-id01"
+d2="hdd-id02"
+flag=`cat "${settingfile}drive.txt"`
+case ${flag} in
+    ${d1} ) ;;
+    ${d2} ) d2=${d1}
+    d1=${flag} ;;
+    * ) flag=${d1} ;;
+esac
+if [ ! -d /media/${d1} ]
+then
+    d1=${d2}
+    d2=${flag}
+fi
+u="9[6-9]%\|100%"
+dck=`df -h | $g ${d1} | $g ${u} | wc -l`
+case ${dck} in
+	1 ) df -h | $g ${d2} | $g ${u} && exit
+	dr=${d2} ;;
+	* ) dr=${d1} ;;
+esac
+cd /media/${dr}/recdir/ || exit
+/usr/local/bin/recpt1 --b25 --strip --sid ${sid} ${1} ${mt} ${1}_${3}_${dt}.ts
+echo "${dr}" > "${settingfile}drive.txt"
+if [ -n "${4}" ]
+then
+  sleep $(($RANDOM%50)) && iepgnum=`cat ${settingfile}iepg.list | sed -e "s/${4}/$(( ${4} + 1 ))/"`
+  echo ${iepgnum} | sed -e "s/\r\|\n//g" -e "s/ /\n/g" > ${settingfile}iepg.list
+fi
