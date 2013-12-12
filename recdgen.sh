024712aaaa2d43eee1eeb5b85e3ba43bf7e9b41d
Mahoroba Shinshi <fyopue@gmail.com>
2013-12-12 13:55:04 +0900

    Update recdgen.sh
    iepgファイルの連番が途切れた時に意図しない番組を予約する問題を回避するようにスイッチを追加、同問題に伴って数百回にわたり繰り返しダウンロードを行なってしまう不具合を修正した。
    crontabで予約番組の末尾にNGと記入すると該当番組の予約登録を削除する機能を追加した（削除の際にiepg.listから番号を探す必要がなくなった）。

--- recdgen.sh
+++ recdgen.sh
@@ -5,7 +5,7 @@ stgfile=${usrdir}recstg/
 reclist=/tmp/
 # エラー処理
 pusherrmsg () {
-  errtime=`date "+%Y/%m/%d %H:%M:%S"`
+  local errtime=`date "+%Y/%m/%d %H:%M:%S"`
   case ${1} in
     1 ) echo "${errtime} : ディレクトリの設定が間違っているようです。末尾にスラッシュがあるか確認してください。" >> /tmp/recdgen_err.log && exit 1 ;;
     2 ) echo "${errtime} : ファイルの取得に失敗しました。iepg.list または ネットワークを確認してください。" ;;
@@ -13,6 +13,8 @@ pusherrmsg () {
     4 ) echo "${errtime} : 予約情報の生成に失敗したようです。設定ファイル、ディレクトリ設定、ネットワークの状態を確認してください。" ;;
     5 ) echo "${errtime} : 番組情報が存在しないため ${3} をリストから削除しました。放送が終了したか、存在しない番組です。" ;;
     6 ) echo "${errtime} : 受信できない番組です。 ${3} をリストから削除しました。" ;;
+    7 ) echo "${errtime} : 意図しない番組予約を行おうとしています。 ${3} をリストから削除しました。番号の間違い、連番の開始位置が変更になった可能性があります。iepg.listを確認・修正してください。" ;;
+    8 ) echo "${errtime} : 意図しない番組予約を行った可能性があります。該当する番号は ${3} です。iepg.listを確認してください。" ;;
   esac >> /tmp/recdgen_err.log
   if [ -z "${3}" ]
   then
@@ -58,8 +60,9 @@ for (( i = 0; i < `cat "${stgfile}iepg.list" | wc -w`; i++ ))
   pgid+=( `cat "${stgfile}iepg.list" | head -$(( ${i} +1 )) | tail -1 | sed -e "s/\r\|\n//g"` )
   getiepg ${reclist} ${pgid[i]}
 # ファイル処理
-  unset data dt tm len nxtm sp
+  unset data dt tm len nxtm sp atpt
   data=(`iepgex "${reclist}" "${pgid[i]}"`)
+  atpt=1
   while :
   do
     if [ "${data[1]}" -lt "1971" ]
@@ -74,6 +77,20 @@ for (( i = 0; i < `cat "${stgfile}iepg.list" | wc -w`; i++ ))
     then
 # データ古い時
       unset data
+      if [ "${atpt}" -eq "4" ]
+      then
+# 4回目で終了
+        data=( `pusherrmsg 7 ${reclist} ${pgid[i]}` )
+        ngid+=( ${pgid[i]} )
+        sp="nodata"
+        break
+      elif [ "${atpt}" -eq "3" ]
+      then
+        pusherrmsg 8 ${reclist} ${pgid[i]}
+        atpt=$(( ${atpt} + 1 ))
+      else
+        atpt=$(( ${atpt} + 1 ))
+      fi
       lsupdate ${stgfile} ${pgid[i]} upd
       rm "${reclist}iepg/iepg.php?PID=${pgid[i]}"
       pgid[i]=$(( ${pgid[i]} + 1 ))
@@ -137,6 +154,13 @@ for (( i = 0; i < `cat "${stgfile}iepg.list" | wc -w`; i++ ))
     else
       tm[1]=$(( ${tm[1]} - 1 ))
     fi
+  fi
+# 削除判定
+  ckdel=`/usr/bin/crontab -l | grep -i "ng$" | grep "${ch[1]} ${len} \"${ttl}\""`
+  if [ -n "${ckdel}" ]
+  then
+    ngid+=( ${pgid[i]} )
+    sp="nodata"
   fi
 # 個別job生成
   case ${sp} in
